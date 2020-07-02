f539bf65b7b24f5505bcb5b65d715ae697214c8e
Tom <thomas.spencer@codurance.com>
2020-07-02 15:19:12 +0100

    Add new User builder

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -43,8 +43,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         .roles("STUDENT") // ROLE_STUDENT
         .build();
 
+    UserDetails lindaUser = User.builder()
+        .username("linda")
+        .password(passwordEncoder.encode("password123"))
+        .roles("ADMIN")
+        .build();
+
     return new InMemoryUserDetailsManager(
-        annaSmithUser
+        annaSmithUser,
+        lindaUser
     );
   }
 }
